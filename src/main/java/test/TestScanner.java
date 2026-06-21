14c9c0c7ff3c9b769fb698c28b8feed6a1e54f15
tarutaruko <tarutaruko39@gmail.com>
2026-06-21 14:00:42 +0900

    共通処理を記入

--- src/main/java/test/TestScanner.java
+++ src/main/java/test/TestScanner.java
@@ -14,14 +14,31 @@ public class TestScanner {
 
      System.out.println("====== CRUDサンプル ======");
      System.out.println("1: CREATE");
-     System.out.println("2: READ");
-     System.out.println("3: UPDATE");
-     System.out.println("4: DELETE");
+     System.out.println("2: UPDATE");
+     System.out.println("3: DELETE");
      System.out.print("選択してください: ");
  
 
      int select = scanner.nextInt();
      
-     System.out.println(select);
+     switch (select) {
+
+     case 1:
+         MySQLMix.insertHobby();
+         break;
+
+     case 2:
+         MySQLMix.updateHobby();
+         break;
+
+     case 3:
+         MySQLMix.deleteHobby();
+         break;
+
+     default:
+         System.out.println("1～3を入力してください");
+     }
+
+     scanner.close();
  }
 }
